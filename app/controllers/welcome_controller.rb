1e2da3b1b6c4ac7a59cf5f7c5e7a0dabcd2fd5fc
Jerry W <jerry.wang.ct@gmail.com>
2016-04-22 21:03:39 -0400

    google api version. needs debugging.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,10 +4,10 @@ class WelcomeController < ApplicationController
 	before_filter :authorize
 
   def index
-		#if @user.Home_Node
+
 
 		@map = CampusMap.new()
-		@result = [[42.365965, -71.25981]]
+
     name_list = Building.order(:name).pluck(:id, :name)
 		adjlist = Path.pluck(:start, :end, :distance)
 		@map.load_map(adjlist)
@@ -16,22 +16,19 @@ class WelcomeController < ApplicationController
     name_list.sort_by{|id, name| name}.each do |id, name|
       @notes[name] = id
     end
-    if params[:start] != nil && params[:end] != nil && params[:start] != '<option value=' && params[:end] != '<option value='
-			@result = []
+		@result = []
+    if params[:start] != nil && params[:end] != nil && params[:start] != '<option value=' && params[:end] != '<option value='		
       paths = @map.solve(params[:start].to_i, params[:end].to_i)
 			str = @map.solve_text(params[:start].to_i, params[:end].to_i)
-			UserMailer.directions_email(User.current_user, str).deliver
+			#UserMailer.directions_email(User.current_user, str).deliver
 			paths.each do |id|
 				node = Node.find(id)
 				@result.push([node.latitude, node.longitude])
 			end
     end
+		puts @result, "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+		return @result
   end
 
-	#def select_home
-		#building_id = params[:select_home]
-		#node = Node.find(building_id)
-		#node.latitude, node.longitude
-	#end
 
 end
